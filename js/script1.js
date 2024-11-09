c8c58b08c78e6460b1f1a3ba0dfe247f9bb8fcc7
THOMAS  SECONDE  DEVIS <kouamedjakiss766@gmail.com>
2024-11-09 13:15:19 +0000

    Create script1.js

---
+++ js/script1.js
@@ -0,0 +1,7 @@
+function redirectToSignup() {
+    window.location.href = 'signup.html'; // Redirige vers la page signup.html
+}
+
+function redirectToSurvey(surveyPage) {
+    window.location.href = surveyPage; // Redirige vers la page de sondage spécifiée
+}
